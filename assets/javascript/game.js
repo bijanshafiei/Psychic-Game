2b8204d9048bbcee1e86fa3dc5f6fe5b11f5ba60
Bijan Shafiei <bshafiei33@gmail.com>
2017-04-28 10:39:06 -0500

    commit 2

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+// Computer chooses letter
+// User presses key and computer compares with chosen letter
+// If users guess does not match computers guess, letter is displayed in guessed so far and guesses left goes down by one
+// If user runs out of guesses left, losses goes up by one and game resets
+// If user guess matches computers guess, wins go up by one
+
+
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+document.onkeyup = function() {
+	var userGuess = String.fromCharCode(event.keyCode).
+		toLowerCase();
+
+		console.log(userGuess);
+
+	var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+
+		console.log(computerGuess);
+
+}
